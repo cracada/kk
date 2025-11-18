2bef02138dcbc0cfd1cab5d3939e33160da94e0f
cracada <2143331+cracada@users.noreply.github.com>
2025-11-18 10:46:14 +0800

    ..

--- run/QuickRun.bat
+++ run/QuickRun.bat
@@ -1,13 +1,11 @@
 @echo off
-if "%~1"=="" (echo 缺少参数 & exit/b)
+if "%~1"=="" (echo no args & exit/b)
 
-if not exist QuickRun.txt (echo 找不到QuickRun.txt & exit/b)
+if not exist %~dp0QuickRun.txt (echo QuickRun.txt Lost & exit/b)
 
-for /f "tokens=1-3 delims=," %%A in (QuickRun.txt) do (
+for /f "tokens=1-3 delims=," %%A in (%~dp0QuickRun.txt) do (
     if /i "%%A"=="%~1" (
         %%B
         exit/b
     )
 )
-
-::echo 未找到参数: %~1
